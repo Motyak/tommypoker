e9005e56111f47c00eb64ba31fba9550a9251aad
Tommy Anfosso <anfossotommy@hotmail.fr>
2024-08-29 23:48:21 +0200

    added print comments

--- main.cpp
+++ main.cpp
@@ -8,24 +8,24 @@ int main()
     {
         auto cards = FULL_SET;
         auto res = HAS_CARD(cards, CLUBS_1)? "true" : "false";
-        std::cout << res << std::endl;
+        std::cout << res << std::endl; // true
     }
     {
         auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1);
         auto res = HAS_CARD(cards, CLUBS_1)? "true" : "false";
-        std::cout << res << std::endl;
+        std::cout << res << std::endl; // false
     }
 
     std::cout << "HAS_CARDS" << std::endl;
     {
         auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1);
         auto res = HAS_CARDS(cards, Cards(HEARTS_1, SPADES_1))? "true" : "false";
-        std::cout << res << std::endl;
+        std::cout << res << std::endl; // true
     }
     {
         auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1);
         auto res = HAS_CARDS(cards, Cards(HEARTS_1, CLUBS_1))? "true" : "false";
-        std::cout << res << std::endl;
+        std::cout << res << std::endl; // false
     }
 
     std::cout << "INTER" << std::endl;
@@ -33,7 +33,7 @@ int main()
         auto cards_A = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
         auto cards_B = Cards(HEARTS_1, CLUBS_1);
         auto cards_C = INTER(cards_A, cards_B);
-        PRINT(cards_C);
+        PRINT(cards_C); // 0000001000000000000000000000000000000100000000000000000000000001
     }
 
     std::cout << "UNION" << std::endl;
@@ -41,7 +41,7 @@ int main()
         auto cards_A = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
         auto cards_B = Cards(HEARTS_1, CLUBS_1);
         auto cards_C = UNION(cards_A, cards_B);
-        PRINT(cards_C);
+        PRINT(cards_C); // 0000001100000000000000000000000000000100000000000010000000000001
     }
 
     std::cout << "DIFF" << std::endl;
@@ -49,7 +49,7 @@ int main()
         auto cards_A = Cards(HEARTS_1, CLUBS_1);
         auto cards_B = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
         auto cards_C = DIFF(cards_A, cards_B);
-        PRINT(cards_C);
+        PRINT(cards_C); // 0000000000000000000000000000000000000000000000000000000000000000
     }
 
     std::cout << "SYMDIFF" << std::endl;
@@ -57,13 +57,13 @@ int main()
         auto cards_A = Cards(HEARTS_1, CLUBS_1);
         auto cards_B = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
         auto cards_C = SYMDIFF(cards_A, cards_B);
-        PRINT(cards_C);
+        PRINT(cards_C); // 0000000100000000000000000000000000000000000000000010000000000000
     }
 
     std::cout << "MISSING" << std::endl;
     {
         auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1, CLUBS_1);
         auto res = MISSING(cards);
-        PRINT(res);
+        PRINT(res); // 0011000000001111111111110111111111111011111111111101111111111110
     }
 }
