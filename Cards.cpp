36d25e80894a8315ee2aa54de5b281367d701e36
Tommy Anfosso <anfossotommy@hotmail.fr>
2024-08-30 18:03:17 +0200

    now using std::accumulate

--- Cards.cpp
+++ Cards.cpp
@@ -1,10 +1,9 @@
 #include <Cards.h>
 
+#include <numeric>
+
 Cards _createCards(std::initializer_list<Card> cards) {
     auto length = cards.size();
-    auto set = uint64_t(0);
-    for (auto card: cards) {
-        set |= uint64_t(1) << card;
-    }
+    auto set = std::accumulate(cards.begin(), cards.end(), uint64_t(0), [](auto set, auto card){return set | (uint64_t(1) << card);});
     return _CREATE_CARDS(length, set);
 }
