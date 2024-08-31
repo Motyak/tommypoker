c67df74bc906e8cd7dca0cfa5e932eaf90da9c41
Motyak <m0tyak_@live.fr>
2024-08-31 16:13:35 +0200

    better

--- Cards.h
+++ Cards.h
@@ -8,19 +8,19 @@
 #include <bit>
 #include <numeric>
 
-using Card = uint8_t; // any "Card" enum which values lie between 0 and 51
+using Card = uint64_t; // any "Card" enum in {2^0, 2^1, ..., 2^51}
 
 /*
 * We are using 8 bytes to store both the set of cards (52 bits..
-* ..for each card enum) and the set length (8 bits for up to 52).
+* ..for each card enum) and the set length (6 bits for up to 52).
 * Chosen bytes format:
 *
-* length (bit 0-7)
-*       |
-*       | unused (bit 8-11)        actual set of cards (bit 12-63)
-*       |       |                               |
-*    -------   --   --------------------------------------------------------------
-*   |       | |  | |                                                              |
+* length (bit 0-5)
+*      |
+*      | unused (bit 6-11)         actual set of cards (bit 12-63)
+*      |      |                                 |
+*    -----  -----   --------------------------------------------------------------
+*   |     ||     | |                                                              |
 *   XXXX'XXXX XXXX'XXXX XXXX'XXXX XXXX'XXXX XXXX'XXXX XXXX'XXXX XXXX'XXXX XXXX'XXXX
 *   0    4    8   12   16        24        32        40        48        56         (64)
 */
@@ -28,28 +28,26 @@ using Cards = uint64_t;
 
 /* constants */
 #define EMPTY_SET 0x0000'0000'0000'0000
-#define FULL_SET 0x340f'ffff'ffff'ffff
+#define FULL_SET 0xd00f'ffff'ffff'ffff
 
 /* constructors/builders */
-#define _CREATE_CARDS(length, set) (uint64_t(length) << 56 | (set))
-#define _CREATE_CARDS_v2(set) _CREATE_CARDS(std::popcount(set), set)
-#define _ADD_TO_SET [](auto set, auto card){return set | (uint64_t(1) << card);}
-#define Cards(...) ({auto list = {__VA_ARGS__}; _CREATE_CARDS(list.size(), std::accumulate(list.begin(), list.end(), uint64_t(0), _ADD_TO_SET));})
+#define _CREATE_CARDS(length, set) (uint64_t(length) << 58 | (set))
+#define Cards(set) _CREATE_CARDS(std::popcount(set), set)
 
 /* selectors */
-#define LENGTH(Cards) ((Cards) >> 56)
-#define UNUSED(Cards) (((Cards) >> 52) & 0xf)
+#define LENGTH(Cards) ((Cards) >> 58)
+#define UNUSED(Cards) (((Cards) >> 52) & 0x3f)
 #define SET(Cards) ((Cards) & 0xf'ffff'ffff'ffff) // 52 last bits <=> 13 last hex-digits
 
-#define HAS_CARD(_Cards, Card) ((_Cards) & (uint64_t(1) << (Card)))
+#define HAS_CARD(_Cards, Card) ((_Cards) & (Card))
 #define HAS_CARDS(_Cards, Cards) ((SET(_Cards) & SET(Cards)) == SET(Cards))
 
-#define INTER(Cards_A, Cards_B) _CREATE_CARDS_v2(SET(Cards_A & Cards_B))
-#define UNION(Cards_A, Cards_B) _CREATE_CARDS_v2(SET(Cards_A | Cards_B))
+#define INTER(Cards_A, Cards_B) Cards(SET(Cards_A & Cards_B))
+#define UNION(Cards_A, Cards_B) Cards(SET(Cards_A | Cards_B))
 // A - B (non-commutative operation)
-#define DIFF(Cards_A, Cards_B) _CREATE_CARDS_v2(SET(Cards_A) - SET(Cards_A & Cards_B))
+#define DIFF(Cards_A, Cards_B) Cards(SET(Cards_A) - SET(Cards_A & Cards_B))
 // each set characteristics (commutative, as opposed to DIFF)
-#define SYMDIFF(Cards_A, Cards_B) _CREATE_CARDS_v2(SET(Cards_A ^ Cards_B))
+#define SYMDIFF(Cards_A, Cards_B) Cards(SET(Cards_A ^ Cards_B))
 
 #define REMAINING(Cards) (Cards)
 #define MISSING(Cards) _CREATE_CARDS(52 - LENGTH(Cards), SET(~Cards))
--- main.cpp
+++ main.cpp
@@ -11,58 +11,58 @@ int main()
         std::cout << res << std::endl; // true
     }
     {
-        auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1);
+        auto cards = Cards(HEARTS_1 | SPADES_1 | DIAMONDS_1);
         auto res = HAS_CARD(cards, CLUBS_1)? "true" : "false";
         std::cout << res << std::endl; // false
     }
 
     std::cout << "HAS_CARDS" << std::endl;
     {
-        auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1);
-        auto res = HAS_CARDS(cards, Cards(HEARTS_1, SPADES_1))? "true" : "false";
+        auto cards = Cards(HEARTS_1 | SPADES_1 | DIAMONDS_1);
+        auto res = HAS_CARDS(cards, Cards(HEARTS_1 | SPADES_1))? "true" : "false";
         std::cout << res << std::endl; // true
     }
     {
-        auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1);
-        auto res = HAS_CARDS(cards, Cards(HEARTS_1, CLUBS_1))? "true" : "false";
+        auto cards = Cards(HEARTS_1 | SPADES_1 | DIAMONDS_1);
+        auto res = HAS_CARDS(cards, Cards(HEARTS_1 | CLUBS_1))? "true" : "false";
         std::cout << res << std::endl; // false
     }
 
     std::cout << "INTER" << std::endl;
     {
-        auto cards_A = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
-        auto cards_B = Cards(HEARTS_1, CLUBS_1);
+        auto cards_A = Cards(HEARTS_1 | CLUBS_1 | DIAMONDS_1);
+        auto cards_B = Cards(HEARTS_1 | CLUBS_1);
         auto cards_C = INTER(cards_A, cards_B);
         PRINT(cards_C); // 0000001000000000000000000000000000000100000000000000000000000001
     }
 
     std::cout << "UNION" << std::endl;
     {
-        auto cards_A = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
-        auto cards_B = Cards(HEARTS_1, CLUBS_1);
+        auto cards_A = Cards(HEARTS_1 | CLUBS_1 | DIAMONDS_1);
+        auto cards_B = Cards(HEARTS_1 | CLUBS_1);
         auto cards_C = UNION(cards_A, cards_B);
         PRINT(cards_C); // 0000001100000000000000000000000000000100000000000010000000000001
     }
 
     std::cout << "DIFF" << std::endl;
     {
-        auto cards_A = Cards(HEARTS_1, CLUBS_1);
-        auto cards_B = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
+        auto cards_A = Cards(HEARTS_1 | CLUBS_1);
+        auto cards_B = Cards(HEARTS_1 | CLUBS_1 | DIAMONDS_1);
         auto cards_C = DIFF(cards_A, cards_B);
         PRINT(cards_C); // 0000000000000000000000000000000000000000000000000000000000000000
     }
 
     std::cout << "SYMDIFF" << std::endl;
     {
-        auto cards_A = Cards(HEARTS_1, CLUBS_1);
-        auto cards_B = Cards(HEARTS_1, CLUBS_1, DIAMONDS_1);
+        auto cards_A = Cards(HEARTS_1 | CLUBS_1);
+        auto cards_B = Cards(HEARTS_1 | CLUBS_1 | DIAMONDS_1);
         auto cards_C = SYMDIFF(cards_A, cards_B);
         PRINT(cards_C); // 0000000100000000000000000000000000000000000000000010000000000000
     }
 
     std::cout << "MISSING" << std::endl;
     {
-        auto cards = Cards(HEARTS_1, SPADES_1, DIAMONDS_1, CLUBS_1);
+        auto cards = Cards(HEARTS_1 | SPADES_1 | DIAMONDS_1 | CLUBS_1);
         auto res = MISSING(cards);
         PRINT(res); // 0011000000001111111111110111111111111011111111111101111111111110
     }
